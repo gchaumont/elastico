951075d48fa0fa88ea84614d6332f29e46a89f43
Guy <gchaumont@me.com>
2023-12-08 09:11:11 +0100

    Index Configuration

--- src/Eloquent/Concerns/HasIndexConfig.php
+++ src/Eloquent/Concerns/HasIndexConfig.php
@@ -8,7 +8,6 @@ use Elastico\Mapping\Field;
 
 trait HasIndexConfig
 {
-
     protected static array $indexConfigurators = [];
 
     abstract protected static function indexConfig(): Config;
@@ -23,20 +22,7 @@ trait HasIndexConfig
 
         $config = static::indexConfig();
 
-        $class = static::class;
-        $booted = [];
-
-        foreach (class_uses_recursive($class) as $trait) {
-            $method = 'config' . class_basename($trait);
-
-            if (method_exists($class, $method) && !in_array($method, $booted)) {
-                forward_static_call([$class, $method], $config);
-
-                $booted[] = $method;
-            }
-        }
-
-        foreach (static::$indexConfigurators[$class] ?? [] as $callback) {
+        foreach (static::$indexConfigurators[static::class] ?? [] as $callback) {
             $callback($config);
         }
 
@@ -48,44 +34,10 @@ trait HasIndexConfig
         static::$indexConfigurators[static::class][] = $callback;
     }
 
-    // {
-    //     return 
-    //     return [
-    //         'index' => (new static())->getTable(),
-    //         'body' => [
-    //             'settings' => static::indexSettings(),
-    //             'mappings' => array_filter([
-    //                 '_source' => static::getSourceSettings(),
-    //                 'dynamic' => static::getDynamicMapping(),
-    //                 'properties' => collect(static::indexProperties())
-    //                     ->keyBy(fn ($prop) => $prop->getName())
-    //                     ->map(fn ($prop) => $prop->toArray())
-    //                     ->all(),
-    //                 // 'dynamic_templates' => static::getDynamicTemplates(),
-    //             ]),
-    //         ],
-    //     ];
-    // }
-
     public static function getFieldNames(): array
     {
         return collect(static::getIndexConfig()->mappings->properties)
             ->map(fn (Field $field) => $field->getName())
             ->all();
     }
-
-    // public static function indexSettings()
-    // {
-    //     return static::$index_settings ??  new stdClass();
-    // }
-
-    // public static function getDynamicMapping(): string
-    // {
-    //     return 'strict';
-    // }
-
-    // public static function getSourceSettings(): array
-    // {
-    //     return [];
-    // }
 }
