a7560c0be78412b950ac76b9d5f7ffad3f1bce12
Guy <gchaumont@me.com>
2024-05-31 13:29:52 +0200

    SoftDeletes

--- src/Eloquent/SoftDeletes.php
+++ src/Eloquent/SoftDeletes.php
@@ -2,9 +2,28 @@
 
 namespace Elastico\Eloquent;
 
+use Elastico\Index\Config;
+use Elastico\Mapping\Field;
+use Carbon\Carbon;
+
+/**
+ * @property Carbon|null $deleted_at
+ * @mixin Model
+ */
 trait SoftDeletes
 {
     use \Illuminate\Database\Eloquent\SoftDeletes;
+
+    public function initializeSoftDeletes(): void
+    {
+        $this->mergeCasts([
+            $this->getDeletedAtColumn() => 'datetime',
+        ]);
+
+        $this->configureIndexUsing(fn (Config $config) => $config->properties(
+            Field::make(name: $this->getDeletedAtColumn(), type: 'date')
+        ));
+    }
 
     /**
      * {@inheritdoc}
